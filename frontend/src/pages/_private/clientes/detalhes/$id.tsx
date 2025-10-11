0b268919c4d113bedeb31dd70bd1d74b69624b50
Lucas Rodrigues <lucasrodriguesbezerra@outlook.com>
2025-10-10 22:30:57 -0300

    feat: init page client details

--- frontend/src/pages/_private/clientes/detalhes/$id.tsx
+++ frontend/src/pages/_private/clientes/detalhes/$id.tsx
@@ -1,9 +1,28 @@
 import { createFileRoute } from '@tanstack/react-router'
+import Loading from '@/components/layout/Loanding';
+import useApi from '@/hooks/useApi';
+import type { ClientGetProps } from '@/schemas/ClientSchema';
 
 export const Route = createFileRoute('/_private/clientes/detalhes/$id')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/clientes/detalhes/"!</div>
+  const { id } = Route.useParams();
+  const { data, isLoading } = useApi<ClientGetProps>({ url: `/users/${id}` });
+
+  if(isLoading) return <Loading />
+
+  console.log(data);
+
+  return (
+    <div>
+      {data?.id}
+      {data?.name}
+      {data?.salary}
+      {data?.companyValuation}
+      {data?.updatedAt}
+      {data?.createdAt}
+    </div>
+  )
 }
