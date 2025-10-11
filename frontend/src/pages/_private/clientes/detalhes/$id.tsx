cd696996cd4a446845e00df63356b5864bd8ab1c
Lucas Rodrigues <lucasrodriguesbezerra@outlook.com>
2025-10-11 11:32:25 -0300

    feat: create generic page details

--- frontend/src/pages/_private/clientes/detalhes/$id.tsx
+++ frontend/src/pages/_private/clientes/detalhes/$id.tsx
@@ -1,7 +1,9 @@
 import { createFileRoute } from '@tanstack/react-router'
-import Loading from '@/components/layout/Loanding';
+import type { ClientProps } from '@/schemas/ClientSchema';
+import { formatToBRL } from '@/helpers/formatToBRL';
+import { formatDate } from '@/helpers/formatDate';
 import useApi from '@/hooks/useApi';
-import type { ClientGetProps } from '@/schemas/ClientSchema';
+import Loading from '@/components/layout/Loanding';
 
 export const Route = createFileRoute('/_private/clientes/detalhes/$id')({
   component: RouteComponent,
@@ -9,18 +11,20 @@ export const Route = createFileRoute('/_private/clientes/detalhes/$id')({
 
 function RouteComponent() {
   const { id } = Route.useParams();
-  const { data, isLoading } = useApi<ClientGetProps>({ url: `/users/${id}` });
+  const { data, isLoading, isError } = useApi<ClientProps>({ url: `/users/${id}` });
 
+  if (isError || !data) return null;
+  
   if(isLoading) return <Loading />
 
   return (
     <div>
-      {data?.id}
-      {data?.name}
-      {data?.salary}
-      {data?.companyValuation}
-      {data?.updatedAt}
-      {data?.createdAt}
+      <p><span className="text-primary">ID:</span> {data.id}</p>
+      <p><span className="text-primary">Nome:</span> {data.name}</p>
+      <p><span className="text-primary">Salário:</span> {formatToBRL(data.salary)}</p>
+      <p><span className="text-primary">Avaliação:</span> {formatToBRL(data.companyValuation)}</p>
+      <p><span className="text-primary">Criado em:</span> {formatDate(data.createdAt)}</p>
+      <p><span className="text-primary">Atualizado em:</span> {formatDate(data.updatedAt)}</p>
     </div>
   )
 }
